3b221c84e66578f83a7a765aa261ee1452b673eb
Romain Debas <debasromain@gmail.com>
2025-10-21 15:48:11 +0200

    components.2

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,14 +1,33 @@
+const pokemonList = [
+    {
+        name: "Bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "Mew",
+    },
+];
+
+
 function PokemonCard() {
+    const pokemon = pokemonList;
     return (
         <section>
-            <figure>
-                <img
-                    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-                    alt="Bulbasaur"
-                />
-            </figure>
-            <figcaption> Bulbasaur </figcaption>
+            {pokemon.map((pokemon, index) =>
+                <div key={index}>
+                    <figure>
+                        {pokemon.imgSrc !== undefined ? (
+                            <img src={pokemon.imgSrc} alt={pokemon.name} />
+                        ) : (
+                            <p> ??? </p>
+                        )}
+                    </figure>
+                    <figcaption> {pokemon.name} </figcaption>
+                </div>
+            )}
         </section>
-    )
+    );
 }
+
 export default PokemonCard;
