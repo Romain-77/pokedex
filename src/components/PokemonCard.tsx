81e54a0c026a7be5f20c73f8cec56a147c7638ec
Romain Debas <debasromain@gmail.com>
2025-10-22 12:01:46 +0200

    props

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,31 +1,14 @@
-const pokemonList = [
-    {
-        name: "Bulbasaur",
-        imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-        name: "Mew",
-    },
-];
-
-
-function PokemonCard() {
-    const pokemon = pokemonList;
+function PokemonCard({ pokemon }) {
     return (
         <section>
-            {pokemon.map((pokemon, index) =>
-                <div key={index}>
-                    <figure>
-                        {pokemon.imgSrc !== undefined ? (
-                            <img src={pokemon.imgSrc} alt={pokemon.name} />
-                        ) : (
-                            <p> ??? </p>
-                        )}
-                    </figure>
-                    <figcaption> {pokemon.name} </figcaption>
-                </div>
-            )}
+            <figure>
+                {pokemon.imgSrc !== undefined ? (
+                    <img src={pokemon.imgSrc} alt={pokemon.name} />
+                ) : (
+                    <p> ??? </p>
+                )}
+            </figure>
+            <figcaption> {pokemon.name} </figcaption>
         </section>
     );
 }
